73e48d29b94cfc34e301512a0e027a6f11877cf6
tsl-ramkrishna <ram_krishna.shrestha@tsl.ac.uk>
2020-10-23 12:34:59 +0100

    script update. codon in RE site is replaced by second best codon, if first codon still gives RE site

--- scripts/mutate_RE_site.py
+++ scripts/mutate_RE_site.py
@@ -3,6 +3,7 @@
 import os, sys
 import argparse
 from Bio import SeqIO
+from operator import itemgetter
 import time
 
 Description="Program to recognise restriction enzyme sites and mutate or replace a nucleotide in the restriction site with another nucleotide such that it does not change the amino acid and the new codon has the highest score of occurrence as shown in the codon file"
@@ -19,7 +20,7 @@ parser.add_argument("--out", action="store", dest="output", default="output.fast
 
 options=parser.parse_args()
 
-
+print(options)
 if not options.codonfile:
     #print "Please provide a codon file showing the occurrences of each codon"
     exit(1)
@@ -44,147 +45,123 @@ def reverse_complement(ntseq):
     return ntseq.translate(str.maketrans(from_this, to_this))
 
 
+def get_restriction_site(re_sites):
 
-#read the restriction enzyme sites
+    #read the restriction enzyme sites
 
-restriction_sites=[]
-restriction_sites_lengths=[]
-fh=open(options.enzymes)
-for line in fh:
-    line=line.rstrip()
-    if line=="": continue
-    linearray=line.split()
-    restriction_sites.append(linearray[1].upper())
-    restriction_sites.append(reverse_complement(linearray[1]).upper())
-    restriction_sites_lengths.append(len(linearray[1]))
+    restriction_sites=[]
+    restriction_sites_lengths=[]
+    fh=open(re_sites)
+    for line in fh:
+        line=line.rstrip()
+        if line=="": continue
+        linearray=line.split()
+        restriction_sites.append(linearray[1].upper())
+        restriction_sites.append(reverse_complement(linearray[1]).upper())
+        restriction_sites_lengths.append(len(linearray[1]))
 
-fh.close()
-#print(restriction_sites)
-#print(restriction_sites_lengths)
-
-min_cut_site_length=min(restriction_sites_lengths)
-max_cut_site_length=max(restriction_sites_lengths)
+    fh.close()
+    return restriction_sites, restriction_sites_lengths
 
 
+def get_codon_scores(codonfile):
 
-#read the codon file first
-aminoacids=dict()
-codons=dict()
-codon_nucleotides={}
-codon_scores={}
-codonfile=open(options.codonfile)
-for line in codonfile:
-    line=line.rstrip()
-    if line=="": continue
+    # read the amino acid codons and their values
+    aminoacids_codons_values = dict()
+    with open(codonfile) as fcon:
+        for line in fcon:
+            line = line.rstrip()
+            if line == "":
+                continue
+            else:
+                linedata = line.split()
+                aa = linedata[0]
+                codon = linedata[1]
+                value = float(linedata[2])
+            
+                if aa in aminoacids_codons_values.keys():
+                    aminoacids_codons_values[aa].update({codon:value})
+                else:
+                    aminoacids_codons_values[aa] = {codon: value}
+    
 
-    linearray=line.split()
-    aa=linearray[0]
-    codon=linearray[1]
-    value=float(linearray[2])
+    return aminoacids_codons_values
 
-    if aa in aminoacids.keys():
-        aminoacids[aa].append(codon)
-    else:
-        aminoacids[aa]=[codon]
-
-    if aa in codons.keys():
-        codons[aa].update({codon:value})
-    else:
-        codons[aa]={codon:value}
-
-    codon_nucleotides[codon]=aa
-    codon_scores[codon]=value
-
-codonfile.close()
-
-#print codons
-#print codon_nucleotides
-
-def get_codon_with_highest_score(aminoacid):
-    score=0
-    aminoacid_codons=aminoacids[aminoacid]
-    for codon in aminoacid_codons:
-        if codon_scores[codon] >= score:
-            score=codon_scores[codon]
-
-    return score
-
-def get_score_for_codon(codon):
-    return codon_scores[codon]
+def sort_codons_by_values(aa_codons):
+    ''' sort aminoacid codons by value:
+    return: list of codon, value pairs
+     '''
+    return sorted(aa_codons.items(), key=itemgetter(1), reverse=True)
 
 def get_codons_for_aminoacid(aminoacid):
-    return aminoacids[aminoacid]
+    ''' get codons for an aminoacid '''
 
-def get_aminoacid_for_codon(codon):
-    for aminoacid in aminoacids.keys():
-        if codon in aminoacids[aminoacid]:
-            return aminoacid
+    for aa in aminoacid_codons_values.keys():
+        if aa == aminoacid:
+            return list(aminoacid_codons_values.keys() )
     return None
 
-def change_cut_site(subseq):
+def get_aminoacid_for_codon(codon):
+    ''' get the aminoacid for a codon '''
 
-    if subseq in restriction_sites:
+    for aa in aminoacid_codons_values.keys():
+        if codon in aminoacid_codons_values[aa].keys():
+            return aa
+        else:
+            continue
+    
+    return None
+
+def change_cut_site(re_subseq, subseq):
+
+    if re_subseq in restriction_sites:
         pass
 
     #check where is the alternate codon with highest score
 
     high_scoring_codons={}
-    #for origcodon in (subseq[:3], subseq[3:6], subseq[6:]):
-    for origcodon in map(''.join, zip(*[iter(subseq)]*3)):
-        aminoacid=codon_nucleotides[origcodon]  # gets aa for that codon
+    #for origcodon in (re_subseq[:3], re_subseq[3:6], re_subseq[6:]):
+    codon_values_in_re_subseq = list()
+    for origcodon in map(''.join, zip(*[iter(re_subseq)]*3)):
+        aminoacid=get_aminoacid_for_codon(origcodon)  # gets aa for that codon
         codons_for_aminoacid=get_codons_for_aminoacid(aminoacid)
-        #print("codons for aminoacid ", aminoacid, " are ", codons_for_aminoacid)
-        #codons_for_aminoacid.delete(codon) # remove the existing codon from the list
-        highest_score=0  #initiate the highest score as 0
-        for aa_codon in codons_for_aminoacid:
-            if aa_codon == origcodon:    # if the codon is same as original codon, don't add
+        
+        # get the aminoacid codons by sorted values in descending order
+        codon_values_in_re_subseq += sort_codons_by_values(aminoacid_codons_values[aminoacid])
+
+    # sort the codons by values from all aminoacids in re_subseq
+    sorted_codon_values_in_re_subseq = sorted(codon_values_in_re_subseq, key=itemgetter(1), reverse=True)
+
+    #remove duplicates from the list, if any
+    sorted_codon_values_in_re_subseq = list(dict.fromkeys(sorted_codon_values_in_re_subseq))
+    print(sorted_codon_values_in_re_subseq)
+    ntseq = ""
+    codon_replaced=False
+    for codon in map(''.join, zip(*[iter(re_subseq)]*3)):
+        aminoacid=get_aminoacid_for_codon(codon)  # gets aa for that codon
+        for codon_in_order, value in sorted_codon_values_in_re_subseq:
+            aa_for_codon_in_order = get_aminoacid_for_codon(codon_in_order)
+            if codon == codon_in_order:
                 continue
+            elif aminoacid == aa_for_codon_in_order and codon_replaced==False:
+                
+                # test if replacing with this codon gives another RE site
+                print("replacing ", codon, " by ", codon_in_order, subseq.replace(codon, codon_in_order))
+                found, re_site = look_in_re_sites(subseq.replace(codon, codon_in_order, 1) )
+                if found == True:
+                    print("Found RE site again. Replacing with second best codon value")
+                    continue
+                else:
+                    ntseq += codon_in_order
+                    codon_replaced=True
+                    break
+
             else:
-                if aa_codon[0:2] == origcodon[0:2]:    #every amino acid has the first two Ns same
-                    codon_score=get_score_for_codon(aa_codon)
-                    if codon_score >= highest_score:     #if existing codon has the highest score, it will also be added
-                        highest_score=codon_score
-                        high_scoring_codons[aa_codon]={"score":highest_score, "original_codon":origcodon}
+                print("Adding nonreplaced codon ", codon)
+                ntseq += codon
+                break
 
-
-    #print("Highest scoring codons ", high_scoring_codons)
-    #lets find the highest scoring codon and its score
-    highest_scoring_codon=""
-    highest_score=0
-    original_codon_at_highest_score=""
-    for codon in high_scoring_codons.keys():
-        if codon == high_scoring_codons[codon]["original_codon"]:   #if original codon has the highest score, just skip it
-            continue
-
-        if high_scoring_codons[codon]["score"] > highest_score:
-            highest_score=high_scoring_codons[codon]["score"]
-            highest_scoring_codon=codon
-            original_codon_at_highest_score=high_scoring_codons[codon]["original_codon"]
-
-
-    #print ("highest scoring codon ", highest_scoring_codon, "highest score ", highest_score, "original codon ", original_codon_at_highest_score)
-    #now lets reconstruct the sequence with highest scoring codon
-    ntseq=""
-    has_changed=False
-    #for codon in list_of_codons_in_frame:
-    for codon in map(''.join, zip(*[iter(subseq)]*3)):
-        #print("codon in frame",codon)
-        if codon == original_codon_at_highest_score and has_changed==False:
-            #print("Changing codon ", codon, " to ", highest_scoring_codon)
-            ntseq+=highest_scoring_codon
-            has_changed=True
-        else:
-            ntseq+=codon
-
-    # now add trailing sequence
-
-    #ntseq=ntseq+bases_left_at_right
-
-    #print("Returning after re mutation", ntseq)
     return ntseq
-
-    #highest_scoring_codon=sorted(high_scoring_codons["score"], key=high_scoring_codons["score"].__getitem__, reverse=True)[0]
-    #highest_score=sorted(high_scoring_codons.values())[0]
 
 def is_re_at_the_end(sequence):
     for seq in restriction_sites:
@@ -210,6 +187,25 @@ def look_in_re_sites(sequence):
 def re_site_start(resite,sequence):
     return sequence.find(resite)
 
+def get_subseq_after_change(subseq, re_start_point, re_end_point):
+    ''' replaces codon in the RE site '''
+    if re_start_point <=3:
+        subseq_to_change = subseq[:6]
+        after_change = change_cut_site(subseq_to_change, subseq)
+        # subseq_after_change = subseq.replace(subseq_to_change, after_change)
+        
+        new_subseq = subseq.replace(subseq_to_change, after_change)
+        print("before change ", "subseq to change ", subseq_to_change, subseq, "After change ", after_change, new_subseq)
+    else:
+        subseq_to_change = subseq[3:]
+        after_change = change_cut_site(subseq_to_change, subseq)
+        # subseq_after_change = subseq.replace(subseq_to_change, after_change)
+        
+        new_subseq = subseq.replace(subseq_to_change, after_change)
+
+        print("before change ", subseq, "subseq to change ", subseq_to_change, "After change ", after_change, new_subseq)
+
+    return subseq_to_change, after_change, new_subseq
 
 def find_re_and_replace_codon(ntseq):
 
@@ -221,35 +217,33 @@ def find_re_and_replace_codon(ntseq):
     while position <= ntseq_length - 1:
         subseq=ntseq[position:position + 9]
         found, re_site = look_in_re_sites(subseq)
-
+      
         if found == True:
-            # print("subseq ", subseq)
+            print("subseq ", subseq)
             re_start_point=re_site_start(re_site, subseq) + 1
             re_end_point = re_start_point + len(re_site) -1
-            # print("Found restriction site ", re_site)
-            # print("RE start point ", re_start_point, " and end point ", re_end_point)
+            print("Found restriction site ", re_site)
+            print("RE start point ", re_start_point, " and end point ", re_end_point)
 
-            if re_start_point <=3:
-                subseq_to_change = subseq[:6]
-                after_change = change_cut_site(subseq_to_change)
-                new_ntseq += subseq.replace(subseq[:6], after_change)
-                # print("before change ", "subseq to change ", subseq_to_change, subseq, "After change ", after_change)
-            else:
-                subseq_to_change = subseq[3:]
-                after_change = change_cut_site(subseq_to_change)
-                new_ntseq += subseq.replace(subseq[3:], after_change)
+            subseq_to_change, subseq_after_change, new_subseq = get_subseq_after_change(subseq, re_start_point, re_end_point)
+            new_ntseq += new_subseq
+            position = len(new_ntseq)
 
-                # print("before change ", subseq, "subseq to change ", subseq_to_change, "After change ", after_change)
-            
-            
-            position=len(new_ntseq)
-
+            time.sleep(1)
         else:
             new_ntseq+=ntseq[position:position+3]
             position +=3
+            
 
     return new_ntseq
 
+
+restriction_sites, restriction_sites_lengths = get_restriction_site(options.enzymes)
+aminoacid_codons_values = get_codon_scores(options.codonfile)
+
+
+min_cut_site_length=min(restriction_sites_lengths)
+max_cut_site_length=max(restriction_sites_lengths)
 
 #now read fasta input sequence file
 fastafile=open(options.input)
